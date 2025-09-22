26c78ba5dccb5cc4384c68d01bdaa9073c438d47
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-09-22 17:26:58 -0600

    feat(database): configure TypeORM with PostgreSQL connection settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true, //TODO: delete in production
     }),
   ],
   controllers: [AppController],
