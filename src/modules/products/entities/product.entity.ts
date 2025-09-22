e855661f66453ce08e600828d530fa877217dee5
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-09-22 17:50:52 -0600

    feat(products): create Product entity

---
+++ src/modules/products/entities/product.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description?: string;
+
+  @Column()
+  price: number;
+
+  @Column({ nullable: true })
+  imageUrl?: string;
+
+  @Column({ unique: true })
+  sku: string;
+
+  @Column({ nullable: true, unique: true })
+  barcode?: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
