f64b5827c1391c62738d53a6e2cf51d2d470f0b2
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-09-24 11:12:42 -0600

    feat(sales): add InventoryModule to sales module imports

--- src/modules/sales/sales.module.ts
+++ src/modules/sales/sales.module.ts
@@ -5,9 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Sale } from './entities/sale.entity';
 import { SaleItem } from './entities/sale-item.entity';
 import { ProductsModule } from '../products/products.module';
+import { InventoryModule } from '../inventory/inventory.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Sale, SaleItem]), ProductsModule],
+  imports: [
+    TypeOrmModule.forFeature([Sale, SaleItem]),
+    ProductsModule,
+    InventoryModule,
+  ],
   controllers: [SalesController],
   providers: [SalesService],
 })
