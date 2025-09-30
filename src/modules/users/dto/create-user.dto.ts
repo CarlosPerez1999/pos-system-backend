338016b91a2df17cef2a5a2259c9f7dc2cb2fd39
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-09-30 10:59:40 -0600

    feat(users): enhance CreateUserDto with API property decorators for Swagger documentation

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -7,19 +7,36 @@ import {
   Length,
 } from 'class-validator';
 import { Role } from '../enum/role.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'Carlos Perez',
+    description: "User's full legal name",
+  })
   @IsString()
   @Length(1, 50)
   name: string;
 
+  @ApiProperty({
+    example: 'carper01',
+    description: 'Unique system identifier for the user',
+  })
   @IsString()
   @Length(1, 20)
   username: string;
 
+  @ApiProperty({
+    example: 'carper01',
+    description: "User's email",
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'StrongP@5s',
+    description: "User's password",
+  })
   @IsStrongPassword({
     minLength: 8,
     minLowercase: 1,
@@ -28,8 +45,17 @@ export class CreateUserDto {
   })
   password: string;
 
+  @ApiProperty({
+    example: true,
+    description: 'User activity status',
+  })
   @IsBoolean()
   isActive: boolean;
+
+  @ApiProperty({
+    example: 'seller',
+    description: 'User role within the system',
+  })
   @IsEnum(Role)
   role: Role;
 }
