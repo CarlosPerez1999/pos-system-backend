708f70054397d29391a480bd5b188766aef200c2
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-09-29 23:07:06 -0600

    fix(dto): ensure limit and offset are parsed as numbers in PaginationDto

--- src/modules/common/dto/pagination.dto.ts
+++ src/modules/common/dto/pagination.dto.ts
@@ -1,16 +1,23 @@
-import { ApiPropertyOptional } from "@nestjs/swagger"
-import { IsNumber, IsOptional } from "class-validator"
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional } from 'class-validator';
 
-export class PaginationDto{
-    @ApiPropertyOptional({example: 10 ,
-    description: 'Maximum number of elements',})
-    @IsOptional()
-    @IsNumber()
-    limit?:number
+export class PaginationDto {
+  @ApiPropertyOptional({
+    example: 10,
+    description: 'Maximum number of elements',
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  limit?: number;
 
-    @ApiPropertyOptional({example: 0 ,
-    description: 'Number of elements to skip from the start',})
-    @IsOptional()
-    @IsNumber()
-    offset?:number
+  @ApiPropertyOptional({
+    example: 0,
+    description: 'Number of elements to skip from the start',
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  offset?: number;
 }
