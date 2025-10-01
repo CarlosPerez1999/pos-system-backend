9218e76a0a0f825563be2ae289c9d337eccd8c73
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-09-30 21:57:48 -0600

    feat(auth): implement authentication service with login and registration methods

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,60 @@
+import {
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import jwt, { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(
+    private readonly usersService: UsersService,
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(loginAuthDto: LoginAuthDto) {
+    try {
+      const { username, password } = loginAuthDto;
+      const user = await this.usersRepository.findOne({
+        where: {
+          username: username,
+        },
+        select: ['id', 'email', 'username', 'password', 'role'],
+      });
+
+      if (!user)
+        throw new NotFoundException(`User with username ${username} not found`);
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) throw new UnauthorizedException('Invalid credentials');
+      const payload = { sub: user.id, username: user.username, role:user.role };
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+      };
+    } catch (error) {
+      this.logger.error(error.message);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async register(createUserDto: CreateUserDto) {
+    return await this.usersService.create(createUserDto);
+  }
+}
