00d3cbddbc2886a36ed5089193d6ba8c247ea656
Carlos Alfredo Pérez Hernández <carlos.al.perez.hdez99@gmail.com>
2025-09-30 22:08:09 -0600

    feat: improve Swagger configuration with JWT authentication

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,17 +14,28 @@ async function bootstrap() {
   );
 
   const swaggerConfig = new DocumentBuilder()
-  .setTitle('POS System API')
-  .setDescription('Documentation for the management API')
-  .setVersion('1.0')
-  .addTag('products', 'Product catalog management')
-  .addTag('sales', 'Sales and payment records')
-  .addTag('users', 'User and Role Management')
-  .addTag('inventory', 'Stock and movement control')
-  .build()
+    .setTitle('POS System API')
+    .setDescription('Documentation for the management API')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'jwt', //strategy name
+    )
+    .addTag('products', 'Product catalog management')
+    .addTag('sales', 'Sales and payment records')
+    .addTag('users', 'User and Role Management')
+    .addTag('inventory', 'Stock and movement control')
+    .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig)
-  SwaggerModule.setup('api', app, documentFactory)
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
